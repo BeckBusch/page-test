d428eafd3c425be6ac004e94208102f38dd195b8
BeckBusch <beckbusch5@gmail.com>
2016-05-17 13:01:57 +1200

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ document.getElementById("hello").addEventListener("click", myFunction);
 
 function myFunction(){
   var happy = prompt("Please enter a combination of 6 characters", "Enter Here");
+   if (happy =! null){
    document.body.style.backgroundColor = happy;
+   }
 }
