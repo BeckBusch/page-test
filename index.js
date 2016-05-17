7f48e74eac15d4f665039e318e7c24eec794d7a2
BeckBusch <beckbusch5@gmail.com>
2016-05-17 12:57:46 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 document.getElementById("hello").addEventListener("click", myFunction);
 
 function myFunction(){
-  var happy = prompt("Please enter a combination of 6 characters" /n/ "the characters can be 1, 2, 3, 4, 5, 6, a, b, c, d, e, or f", "Enter Here");
-if (happy != null) {
-    document.body.style.color = happy;
+  var happy = prompt("Please enter a combination of 6 characters", "Enter Here");
+   document.body.style.color = happy;
 }
-}
-
-/*function myFunction(){
-  alert('hi');
-}*/
